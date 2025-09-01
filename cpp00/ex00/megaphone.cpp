3f5337d20e80028956c917d499f2b120e3ca794f
mid-taoufiq <taoufiqibariken@gmail.com>
2025-09-01 08:07:23 +0100

    start

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char const *argv[])
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        for (int i = 1; argv[i]; i++)
+        {
+            for (int j = 0; argv[i][j]; j++)
+            {
+                std::cout << (char)std::toupper(argv[i][j]);
+            }
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
