da7a3600594c297ba10619c3b0f61fad889bc87c
mid-taoufiq <taoufiqibariken@gmail.com>
2025-09-11 22:15:57 +0100

    ...

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,26 +3,30 @@
 
 int main(int argc, char *argv[])
 {
-	std::string text;
-
-	std::string outfile_name = argv[1] + ".replace";
-	size_t found_index;
-	std::ifstream infile(argv[1]);
-
-	std::ofstream outfile();
-	if (argc != 4 )
+	if (argc != 4)
 	{
 		std::cerr << "arguments error" << std::endl;
 		return (1);
 	}
+	std::string text;
+	std::size_t found_index = 0;
+	std::size_t start = 0;
+	std::string replaced_str = argv[2];
+	std::ifstream infile(argv[1]);
+	std::string outfile_tmp = argv[1];
+	std::string outfile_name = outfile_tmp + ".replace";
+	std::ofstream outfile(outfile_name);
 	if (!getline(infile, text, '\0'))
 	{
 		std::cerr << "getline fail" << std::endl;
 		return (1);
 	}
-	while ((found_index = text.find(argv[2])) != std::string::npos)
+	while ((found_index = text.find(replaced_str, start)) != std::string::npos)
 	{
-		std::
+		outfile << text.substr(start, found_index - start);
+		outfile << argv[3];
+		start = (found_index + replaced_str.length());
 	}
+	outfile << text.substr(start, found_index - start);
 	return 0;
 }
